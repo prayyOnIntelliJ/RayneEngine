c58b869008b5babebab69d6af46fde2ad46bd86e
prayyOnIntelliJ <122681300+prayyOnIntelliJ@users.noreply.github.com>
2026-03-10 16:17:34 +0100

    Added ScriptComponent with onCreate and onUpdate functions

--- src/Core/Scripting/ScriptComponent.cpp
+++ src/Core/Scripting/ScriptComponent.cpp
@@ -1,1 +1,47 @@
 #include "../Scripting/ScriptComponent.h"
+
+ScriptComponent::ScriptComponent(sol::state& lua, const std::string& path)
+    : m_Lua(&lua)
+{
+    m_Env = sol::environment(*m_Lua, sol::create, m_Lua->globals());
+
+    sol::load_result loadResult = m_Lua->load_file(path);
+
+    if (!loadResult.valid())
+    {
+        sol::error err = loadResult;
+        std::cerr << "Lua Load Error (" << path << "): " << err.what() << std::endl;
+        return;
+    }
+
+    sol::protected_function scriptFunc = loadResult;
+
+    sol::set_environment(m_Env, scriptFunc);
+
+    sol::protected_function_result execResult = scriptFunc();
+
+    if (!execResult.valid())
+    {
+        sol::error err = execResult;
+        std::cerr << "Lua Execution Error (" << path << "): " << err.what() << std::endl;
+        return;
+    }
+
+    m_OnCreate = m_Env["OnCreate"];
+    m_OnUpdate = m_Env["OnUpdate"];
+}
+
+void ScriptComponent::OnCreate() const
+{
+    if (m_OnCreate.valid()) m_OnCreate();
+}
+
+void ScriptComponent::OnUpdate(float dt) const
+{
+    if (m_OnUpdate.valid()) m_OnUpdate(dt);
+}
+
+void ScriptComponent::SetEntity(Entity e)
+{
+    m_Env["self"] = e;
+}
--- src/Core/Scripting/ScriptComponent.h
+++ src/Core/Scripting/ScriptComponent.h
@@ -4,17 +4,21 @@
 #include <sol/sol.hpp>
 #include <string>
 
+#include "../ECS/Entity.h"
+
 class ScriptComponent
 {
 public:
     ScriptComponent(sol::state& lua, const std::string& path);
 
-    void OnCreate();
-    void OnUpdate(float dt);
+    void OnCreate() const;
+    void OnUpdate(float dt) const;
+    void SetEntity(Entity e);
+    sol::environment& GetEnv() { return m_Env; }
 
 private:
-    sol::state& m_Lua;
-    sol::table m_Env;
+    sol::environment m_Env;
+    sol::state* m_Lua;
 
     sol::function m_OnCreate;
     sol::function m_OnUpdate;
