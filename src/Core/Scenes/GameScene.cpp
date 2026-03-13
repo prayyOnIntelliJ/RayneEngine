68b5b51dc696b6c5bae75e6d1074734b7493d95f
prayy <122681300+prayyOnIntelliJ@users.noreply.github.com>
2026-03-13 14:51:31 +0100

    Added GameScene with functionality

---
+++ src/Core/Scenes/GameScene.cpp
@@ -0,0 +1,126 @@
+﻿#include "GameScene.h"
+#include "../Scenes/SceneManager.h"
+#include <iostream>
+
+#include "../ECS/Components.h"
+#include "../Scripting/EventManager.h"
+#include "../Scripting/ScriptComponent.h"
+#include "SFML/Graphics/RectangleShape.hpp"
+#include "SFML/Window/Event.hpp"
+
+GameScene::GameScene(SceneManager& manager, sf::RenderWindow& window, Registry& registry)
+    : Scene(manager), m_Window(window), m_Registry(registry)
+{
+    m_Font.loadFromFile(ASSET_PATH "fonts/Merriweather.ttf");
+
+    m_DebugText.setFont(m_Font);
+    m_DebugText.setCharacterSize(12);
+    m_DebugText.setFillColor(sf::Color(180, 180, 180));
+    m_DebugText.setPosition(8.f, 8.f);
+}
+
+void GameScene::OnEnter()
+{
+    std::cout << "[GameScene] Started\n";
+
+    m_Camera = m_Window.getDefaultView();
+
+    EventManager::Get().SubscribeCollision([this](CollisionEvent e)
+    {
+        if (m_Registry.HasComponent<ScriptComponent>(e.a))
+            m_Registry.GetComponent<ScriptComponent>(e.a).OnCollision(e.b);
+
+        if (m_Registry.HasComponent<ScriptComponent>(e.b))
+            m_Registry.GetComponent<ScriptComponent>(e.b).OnCollision(e.a);
+    });
+
+    m_Registry.ForEach<ScriptComponent>([](Entity, ScriptComponent& sc)
+    {
+        sc.OnCreate();
+    });
+}
+
+void GameScene::OnExit()
+{
+    std::cout << "[GameScene] Stopped\n";
+}
+
+void GameScene::CheckCollisions()
+{
+    struct CollidableEntity { Entity id; float x, y, w, h; };
+    std::vector<CollidableEntity> collidables;
+
+    m_Registry.ForEach<TransformComponent, RenderComponent>(
+        [&collidables](Entity e, TransformComponent& t, RenderComponent& r)
+        {
+            collidables.push_back({ e, t.x, t.y, r.size.x, r.size.y });
+        });
+
+    std::vector<std::pair<Entity, Entity>> currentCollisions;
+
+    for (size_t i = 0; i < collidables.size(); i++)
+    {
+        for (size_t j = i + 1; j < collidables.size(); j++)
+        {
+            const auto& a = collidables[i];
+            const auto& b = collidables[j];
+
+            const bool overlapping =
+                a.x < b.x + b.w && a.x + a.w > b.x &&
+                    a.y < b.y + b.h && a.y + a.h > b.y;
+
+            if (!overlapping) continue;
+
+            currentCollisions.emplace_back(a.id, b.id);
+
+            const bool wasColliding = std::find(
+                m_LastCollisions.begin(), m_LastCollisions.end(),
+                std::make_pair(a.id, b.id)) != m_LastCollisions.end();
+
+            if (!wasColliding)
+                EventManager::Get().FireCollision(a.id, b.id);
+        }
+    }
+}
+
+void GameScene::HandleEvent(const sf::Event& event)
+{
+    if (event.type == sf::Event::KeyPressed &&
+        event.key.code == sf::Keyboard::Escape)
+    {
+        m_manager.SwitchSceneTo("editor");
+    }
+}
+
+void GameScene::Update(float deltaTime)
+{
+    m_Registry.ForEach<TransformComponent, VelocityComponent>(
+        [deltaTime](Entity, TransformComponent& t, VelocityComponent& v)
+        {
+            t.x += v.dx * deltaTime;
+            t.y += v.dy * deltaTime;
+        });
+
+    m_Registry.ForEach<ScriptComponent>([deltaTime](Entity, ScriptComponent& sc)
+    {
+        sc.OnUpdate(deltaTime);
+    });
+}
+
+void GameScene::Render(sf::RenderWindow& window)
+{
+    window.setView(m_Camera);
+
+    m_Registry.ForEach<TransformComponent, RenderComponent>(
+        [&window](Entity, TransformComponent& t, RenderComponent& r)
+        {
+            sf::RectangleShape shape(r.size);
+            shape.setPosition(t.x, t.y);
+            shape.setFillColor(r.color);
+            window.draw(shape);
+        });
+
+    window.setView(window.getDefaultView());
+    m_DebugText.setString("GAME  |  Esc: back to editor");
+    window.draw(m_DebugText);
+}
---
+++ src/Core/Scenes/GameScene.h
@@ -0,0 +1,33 @@
+﻿#ifndef RAYNEENGINE_GAMESCENE_H
+#define RAYNEENGINE_GAMESCENE_H
+#include "Scene.h"
+#include "../ECS/Registry.h"
+#include "SFML/Graphics/Text.hpp"
+
+
+class GameScene : public Scene
+{
+public:
+    GameScene(SceneManager& manager, sf::RenderWindow& window, Registry& registry);
+
+    void HandleEvent(const sf::Event& event) override;
+    void Update(float deltaTime) override;
+    void Render(sf::RenderWindow& window) override;
+
+    void OnEnter() override;
+    void OnExit() override;
+
+private:
+    sf::RenderWindow& m_Window;
+    Registry& m_Registry;
+
+    sf::View m_Camera;
+    sf::Font m_Font;
+    sf::Text m_DebugText;
+
+    void CheckCollisions();
+    std::vector<std::pair<Entity, Entity>> m_LastCollisions;
+};
+
+
+#endif //RAYNEENGINE_GAMESCENE_H
