5f5080bbfa892568e70cbe5f9f71e4f45b155c03
prayy <122681300+prayyOnIntelliJ@users.noreply.github.com>
2026-03-18 08:54:33 +0100

    Added SpriteComponent

--- src/Core/ECS/Components.h
+++ src/Core/ECS/Components.h
@@ -4,6 +4,9 @@
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/System/Vector2.hpp>
 
+#include "SFML/Graphics/Sprite.hpp"
+#include "SFML/Graphics/Texture.hpp"
+
 struct TransformComponent
 {
     float x, y;
@@ -18,6 +21,29 @@ struct RenderComponent
 {
     sf::Color color;
     sf::Vector2f size;
+};
+
+struct SpriteComponent
+{
+    std::string texturePath;
+    std::shared_ptr<sf::Texture> texture;
+    sf::Sprite sprite;
+    sf::Vector2f size;
+
+    explicit SpriteComponent(const std::string& path, sf::Vector2f size)
+        : texturePath(path), size(size)
+    {
+        texture = std::make_shared<sf::Texture>();
+        if (texture->loadFromFile(path))
+        {
+            sprite.setTexture(*texture);
+            const sf::Vector2u texSize = texture->getSize();
+            sprite.setScale(
+                size.x / static_cast<float>(texSize.x),
+                size.y / static_cast<float>(texSize.y)
+            );
+        }
+    }
 };
 
 #endif
